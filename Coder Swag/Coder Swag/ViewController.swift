cd669ce0d6ddb589f2b87d23eedb53cf9c814691
Daniel Winship <danielwinship@icloud.com>
2018-01-03 06:14:30 -0500

    init commit

---
+++ Coder Swag/Coder Swag/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coder Swag
+//
+//  Created by Daniel Winship on 1/3/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
