b3802d26519b62087e53baac82bc6bc8816a0a61
Daniel Winship <danielwinship@icloud.com>
2018-01-04 06:38:54 -0500

    Created collection VC and prototype collection view cells

---
+++ Coder Swag/Coder Swag/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  Coder Swag
+//
+//  Created by Daniel Winship on 1/4/18.
+//  Copyright © 2018 Daniel Winship. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage:UIImageView!
+    @IBOutlet weak var productTitle:UILabel!
+    @IBOutlet weak var productPrice:UILabel!
+    
+}
